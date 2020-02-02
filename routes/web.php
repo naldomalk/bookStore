4c715065bdc5347ce8a624368cf8b3a555332233
Arnaldo Liberal dos Santos <arnaldo@ahcom.com.br>
2020-02-02 00:32:55 -0200

    updated routes - API

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,11 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers;
 
-Route::get('/', function () { return 'bookStore API Running.'; });
+Route::get('/', function () { return ''; }); // TODO front-end
+
+Route::get('/status', function () { return 'bookStore API Running.'; }); // TODO front-end
+
+Route::get('/api/books', function (Controllers\BooksController $controller) { return $controller->getMany(); });
+Route::get('/api/books/{id}', function (Controllers\BooksController $controller) { return $controller->getOne(); });
+Route::post('/api/books', function (Controllers\BooksController $controller) { return $controller->create(); });
+Route::delete('/api/books/{id}', function (Controllers\BooksController $controller) { return $controller->delete(); });
