d0c3e0f7fd12fa2b31a4eec57e2806e5c9447839
Arnaldo Liberal dos Santos <arnaldo@ahcom.com.br>
2020-02-02 11:38:48 -0200

    Simplify routesm - API

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
-
 use Illuminate\Http\Request;
 use App\Http\Controllers;
 
 Route::get('/', function () { return ''; }); // TODO front-end
 
-Route::get('/status', function () { return 'bookStore API Running.'; }); // TODO front-end
+Route::get('/status', function () { return 'bookStore app Running.'; });
 
-Route::get('/api/books', function (Controllers\BooksController $controller) { return $controller->getMany(); });
-Route::get('/api/books/{id}', function (Controllers\BooksController $controller) { return $controller->getOne(); });
-Route::post('/api/books', function (Controllers\BooksController $controller) { return $controller->create(); });
-Route::delete('/api/books/{id}', function (Controllers\BooksController $controller) { return $controller->delete(); });
+// api calls
+Route::get('api/books', 'BooksController@getMany' );
+Route::get('api/books/{id}', 'BooksController@getOne' );
+Route::post('api/books', 'BooksController@create' );
